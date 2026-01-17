94f3afff7d43c91be9f693b693ad66fa9e4cae1e
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2026-01-17 15:26:17 +0100

    feat(router): implement centralized 404 handling and method spoofing

--- src/Core/Router/Router.php
+++ src/Core/Router/Router.php
@@ -2,47 +2,58 @@
 
 namespace App\Core\Router;
 
-class Router {
+class Router
+{
     protected static $routes = [];
 
-    public static function load($file) {
-        require $file;
-        return new static;
-    }
-
-    public static function get($uri, $controller) {
+    public static function get($uri, $controller)
+    {
         self::$routes['GET'][$uri] = $controller;
     }
 
-    public static function post($uri, $controller) {
+    public static function post($uri, $controller)
+    {
         self::$routes['POST'][$uri] = $controller;
     }
 
-    public static function resolve() {
+    public static function resolve()
+    {
         $method = $_SERVER['REQUEST_METHOD'];
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+        // 1. Handling dyal Method Spoofing (PUT/DELETE)
         if ($method === 'POST' && isset($_POST['_method'])) {
             $method = strtoupper($_POST['_method']);
         }
 
-        if (isset(self::$routes[$method][$uri])) {
-            $callback = self::$routes[$method][$uri];
+        // 2. Check ila l-route kany f l-array dyalna
+        if (!isset(self::$routes[$method][$uri])) {
+            http_response_code(404);
+            // Had l-view errors/404 khass t-kon dertiha f views/errors/404.med.php
+            return view('errors/404', ['title' => 'Page Not Found']);
+        }
 
-            if (is_array($callback)) {
-                $controllerName = $callback[0];
-                $methodName = $callback[1];
-                
-                if (class_exists($controllerName)) {
-                    $controller = new $controllerName();
+        $callback = self::$routes[$method][$uri];
+
+        // 3. Execute l-Controller o l-Method
+        if (is_array($callback)) {
+            [$controllerName, $methodName] = $callback;
+
+            if (class_exists($controllerName)) {
+                $controller = new $controllerName();
+                if (method_exists($controller, $methodName)) {
                     return $controller->$methodName();
                 }
             }
-            
+        }
+
+        // 4. Fallback l-Closures (ila knti dayr callable f routes)
+        if (is_callable($callback)) {
             return call_user_func($callback);
         }
 
+        // Final fallback 404
         http_response_code(404);
-        echo "404 - Page Not Found";
+        return view('errors/404', ['title' => 'Error']);
     }
 }
