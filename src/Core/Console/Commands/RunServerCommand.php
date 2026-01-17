ecf52f39b81fd31481c3d9258bbb856a03b2a874
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2026-01-17 21:37:35 +0100

    feat(console): add run:server command and register it in CLI

---
+++ src/Core/Console/Commands/RunServerCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Core\Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class RunServerCommand
+ * Starts the PHP built-in development server.
+ */
+class RunServerCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('run:server')
+            ->setDescription('Start the PHP development server');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln("<info>Med Framework server starting on http://localhost:8000</info>");
+        $output->writeln("<comment>Press Ctrl+C to stop the server</comment>");
+
+        // Execute the built-in PHP server pointing to the public directory
+        passthru("php -S localhost:8000 -t public");
+
+        return Command::SUCCESS;
+    }
+}
