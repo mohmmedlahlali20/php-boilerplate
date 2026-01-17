6213394a5a2309a8b8e4c6704a9e314d5ff67f29
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2026-01-17 15:44:07 +0100

    feat(ui): add asset helper and global css/js loading with @yield('scripts')

--- views/users/list.med.php
+++ views/users/list.med.php
@@ -1,9 +1,12 @@
 @extends('layouts/master')
-@section('main_content')
-    <h1 class="text-3xl font-bold text-blue-600">User Management</h1>
+    @section('main_content')
+            <h1 class="text-3xl font-bold text-blue-600">User Management</h1>
+                @foreach($users as $user)
+                    <p>{{ $user['name'] }}</p>
+                @endforeach
     @endsection
 
-@section('scripts')
+    @section('scripts')
     <script>
         console.log("Hada script gha-y-khdem ghir f had l-page!");
     </script>
