3df52dc03fedf34ecd7bb1d3700759b464240c80
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2026-01-17 15:26:48 +0100

    style(layout): fix sidebar and navbar positioning with tailwind

--- views/layouts/master.med.php
+++ views/layouts/master.med.php
@@ -1,6 +1,31 @@
-<html>
-<head><title>{{ $title ?? 'Default' }}</title></head>
-<body>
-    @yield('main_content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'NAN' }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-50">
+
+    @include('partials/nav')
+
+    <div class="flex overflow-hidden bg-white pt-16">
+        
+        @include('partials/sidebar')
+
+        <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+            <main>
+                <div class="pt-6 px-4 min-h-[calc(100vh-120px)]">
+                    @yield('main_content')
+                </div>
+            </main>
+            
+            @include('partials/footer')
+        </div>
+
+    </div>
+
+    @yield('scripts')
 </body>
 </html>
