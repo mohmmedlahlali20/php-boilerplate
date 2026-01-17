38da3f2a04d0281a08d34a530c0de9bd37e7f2d8
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2026-01-17 15:26:54 +0100

    fix(view): update user list to extend master layout correctly

--- views/users/list.med.php
+++ views/users/list.med.php
@@ -1,8 +1,10 @@
 @extends('layouts/master')
-
 @section('main_content')
-    <h1>Lista dyal l-Users</h1>
-    @foreach($users as $user)
-        <p>{{ $user['name'] }}</p>
-    @endforeach
+    <h1 class="text-3xl font-bold text-blue-600">User Management</h1>
+    @endsection
+
+@section('scripts')
+    <script>
+        console.log("Hada script gha-y-khdem ghir f had l-page!");
+    </script>
 @endsection
