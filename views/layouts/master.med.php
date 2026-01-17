960677761d3fc8d098c5419f1a031e822aa089c6
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2026-01-17 21:37:57 +0100

    refactor(views): update master layout structure and documentation

--- views/layouts/master.med.php
+++ views/layouts/master.med.php
@@ -4,18 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'NAN' }}</title>
+    <title>{{ $title ?? 'Med Framework' }}</title>
     <link rel="stylesheet" href="{{ asset('resources/css/global.css') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-50">
 
-    <!-- @include('partials/nav') -->
-
     <div class="flex overflow-hidden bg-white pt-16">
-
-        <!-- @include('partials/sidebar') -->
 
         <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
             <main>
@@ -24,8 +20,7 @@
                 </div>
             </main>
 
-            <!-- @include('partials/footer') -->
-        </div>
+            </div>
 
     </div>
     <script src="{{ asset('resources/javascript/main.js') }}"></script>
