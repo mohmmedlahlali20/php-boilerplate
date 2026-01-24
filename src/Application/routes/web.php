28bcd86951daae7ef6ae303ceea3a38f6268982a
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2026-01-24 16:45:27 +0100

    chore(routes): update web routes

--- src/Application/routes/web.php
+++ src/Application/routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Core\Router\Router;
-use App\Core\Bootstrap\Bootstrap;
-// use App\Application\Controllers\UserController;
 
-// Router::get('getAllUsers', [UserController::class, 'index']);
 Router::get('/', function() {
-    return dd('tir bzkk');
+    return render('home', ['title' => 'Welcome to Med Framework']);
 });
+
+Router::get('/docs', function() {
+    return render('docs', ['title' => 'Documentation']);
+});
+
+Router::get('/showcase', function() {
+    return render('showcase', ['title' => 'UI Showcase']);
+});
+
